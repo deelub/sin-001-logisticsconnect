27262bd45a7f14b98224d84b44ed3af3781f885f
divine <divinelubiku6@gmail.com>
2026-09-21 12:18:41 +0200

    feat : modelRecords tom parse CSV

--- ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
+++ ingestion-service/src/main/java/co/wethinkcode/logisticsconnect/IngestionServiceApp.java
@@ -1,8 +1,47 @@
 package co.wethinkcode.logisticsconnect;
 
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import io.javalin.Javalin;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngestionServiceApp {
+
+    private static List<modelRecords> records = new ArrayList<>();
+
+    public static class modelRecords {
+        private final String hubID;
+        private final String hubName;
+        private final String province;
+        private final String district;
+
+        public modelRecords(String hubID, String hubName, String province, String district){
+            this.hubID = hubID;
+            this.hubName = hubName;
+            this.province = province;
+            this.district= district;
+
+        }
+
+    }
+
+    public static List<modelRecords> cleanFile(String filename){
+
+        try(InputStream is= IngestionServiceApp.class.getResourceAsStream(filename);
+        InputStreamReader isr = new InputStreamReader(is);
+        CSVReader csvReader = new CSVReaderBuilder(isr).withSkipLines(1).build())
+        {
+
+
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7050);
